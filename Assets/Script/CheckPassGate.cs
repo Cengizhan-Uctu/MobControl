15af3805fc51488826111c6d8c9d15148f65010d
Cengizhan-Uctu <77956850+Cengizhan-Uctu@users.noreply.github.com>
2022-03-19 20:26:15 +0300

    Gate bug fixing

--- Assets/Script/CheckPassGate.cs
+++ Assets/Script/CheckPassGate.cs
@@ -13,26 +13,31 @@ public class CheckPassGate : MonoBehaviour
 
         gates = GameObject.FindGameObjectsWithTag("Gate");
 
-      
-        for (int i = 0; i < gates.Length - 1; i++)
+        for (int i = 0; i < gates.Length; i++)
         {
             gatesNameList.Add(gates[i].name);
+            Debug.Log(gates[i].name);
         }
 
+    }
+   
+    private void OnTriggerExit(Collider other)
+    {
+       
+        if (gatesNameList.Contains(other.name) && other.CompareTag("Gate"))
+        {
+            for (int i =0; i<other.GetComponent<GateStatus>().multiplier-1;i++) 
+            {
+                GameObject newchlid = Instantiate(gameObject, transform.position, Quaternion.identity);
+                newchlid.GetComponent<CheckPassGate>().listdegress(other.name);
+                listdegress(other.name);
+            }
+            
+        }
     }
     public void listdegress(string name)
     {
         gatesNameList.Remove(name);
 
-    }
-    private void OnTriggerExit(Collider other)
-    {
-      
-        if (gatesNameList.Contains(other.name) && other.CompareTag("Gate"))
-        {
-          
-            GameObject newchlid = Instantiate(gameObject, transform.position, Quaternion.identity);
-            newchlid.GetComponent<CheckPassGate>().listdegress(other.name);
-        }
     }
 }
